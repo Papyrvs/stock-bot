346c330bfd6a201ed027e44304b159991db978a2
CarixoHD <36801999+CarixoHD@users.noreply.github.com>
2020-03-23 23:05:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modules import stock, password
+from modules import stock, password, check
 import prettytable
 import platform
 from tkinter import Tk
@@ -11,10 +11,10 @@ def price_check(ticker, headers, credentials, objs):
     try:
         while True:
             for tickers in ticker:
-                price = check(tickers, table)
+                price = check.check (tickers, table)
 
                 if float(price) < 3.2:
-                    objs[tickers].buy()
+                    objs[tickers].stock.buy()
     except KeyboardInterrupt:
         pass
 
@@ -22,10 +22,12 @@ def main():
     credentials = {'username': "FlyingRainbowPotato", 'password': password.pw()}
 
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
-    TICKER = ['TSLA']
-    objs = {tickers: stock.Stock(credentials = credentials, ticker = tickers) for tickers in TICKER}
+    TICKER = [ "USD/CHF"]
+    #objs = {tickers: stock.Stock(credentials = credentials, ticker = tickers) for tickers in TICKER}
 
-    price_check(TICKER, headers, credentials, objs)
+    #price_check(TICKER, headers, credentials, objs)
+    test = check.ticker(["USD/CHF", "TSLA"])
+    print(test.ticker)
 
 if __name__ == "__main__":
     main()
