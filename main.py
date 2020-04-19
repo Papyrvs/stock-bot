a3560bec687cc460637ea17774b043f4084ae7b9
CarixoHD <shayking7yt@gmail.com>
2020-04-19 20:34:12 +0200

    new api for degiro

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-from modules import stock, password, tick
-import prettytable
-
-def price_check(tickers, table, obj):
-    try:
-        while True:
-            price = tickers.check(table) 
-            for key, value in price.items():
-                if float(value) < 4:
-                    tickers.buy(key, obj)
-
-    except KeyboardInterrupt:
-        pass
+# import sys
+# import os
+# sys.path.insert(0, '%s%s' % (os.path.dirname(os.path.realpath(__file__)), '/modules/'))
+# from degiro import *
+import sys
+from modules import degiro
 
 def main():
-    table = prettytable.PrettyTable()
-    table.field_names = ["Ticker", "Price in USD"]
-    credentials = {'username': "FlyingRainbowPotato", 'password': password.pw()}  
-    tickers = tick.ticker(["USD/CHF", "TSLA"]) # INFO: If fiat - FORMAT: USD/'currency'
-    objs = {tics: stock.Stock(credentials = credentials, ticker = tics) for tics in tickers.dticker}
+    tickers = ['NIO', 'TSLA'] # INFO: If fiat - FORMAT: USD/'currency'
     
-    #objs[tickers[0]].buy()
-    price_check(tickers, table, objs)
+    # NOTE: ALL RETURNS ARE DICTIONARIES
+    
+    obj = degiro.Degiro() # Creates an instance of degiro.Degiro()
+    
+    ticker_data = obj.getTickerData(tickers) # Gets information about the inputted tickers
+    print(ticker_data)
+    
+    ticker_price = obj.getCurrentPrice(tickers) # Gets current price of the given stock/stocks
+    print(ticker_price)
+    
+    funds = obj.getCashFunds() # Returns your current cash funds
+    print(funds)
+    
+    portfolio = obj.getPortfolio() # Returns detailed info about your portfolio
+    print(portfolio)
+    
+    
+    # TODO:
+    # - Buy/sell stocks
+    # - Support currency
 
+
+
+
+    # NOT IN USE
+
+        # objs = {tics: stock.Stock(credentials = credentials, ticker = tics) for tics in tickers.dticker}
+        # #objs[tickers[0]].buy()
+        # price_check(tickers, table, objs)
+        
+        
+        
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nBye bye')
