b6c2fb932ff4a77249bd5b2e0b649b47564de0b4
Shayan Alinejad <shayking7yt@gmail.com>
2020-04-28 23:37:42 +0200

    optimized search

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import sys
-from Degiro import *
+from Degiro import Degiro
+
 
 def main():
-    tickers = ['USD/CHF', 'TSLA'] # INFO: If fiat - FORMAT: USD/'currency'
-    stockAmount = {'NIO': 5, 'TSLA': 4} # Key is the stock, and value is the amount
+    tickers = ['USD/CHF', 'TSLA']  # INFO: If fiat - FORMAT: USD/'currency'
+    # Key is the stock, and value is the amount
+    stockAmount = {'NIO': 5, 'TSLA': 4}
 
     # NOTE: ALL RETURNS ARE DICTIONARIES
 
-    obj = Degiro() # Creates an instance of degiro.Degiro()
+    obj = Degiro()  # Creates an instance of degiro.Degiro()
 
-    ticker_data = obj.getTickerData(tickers) # Gets information about the inputted tickers
+    # Gets information about the inputted tickers
+    ticker_data = obj.getTickerData(tickers)
     print(ticker_data)
-    
-    ticker_price = obj.getCurrentPrice(tickers) # Gets current price of the given stock/stocks
+
+    # Gets current price of the given stock/stocks
+    ticker_price = obj.getCurrentPrice(tickers)
     print(ticker_price)
-    
-    funds = obj.getCashFunds() # Returns your current cash funds
+
+    funds = obj.getCashFunds()  # Returns your current cash funds
     print(funds)
 
-    portfolio = obj.getPortfolio() # Returns detailed info about your portfolio
+    portfolio = obj.getPortfolio()  # Returns detailed info about your portfolio
     print(portfolio)
 
-    # NOTE: DOES NOT RETURN ANYTHING. 
+    # NOTE: DOES NOT RETURN ANYTHING.
     # IT IS ONLY A SIMULATION
+    for _ in range(10):
 
-    obj.testBuy(stockAmount) # Buys stocks. Key is the stock it is buying, while the value is the amount
+        # Buys stocks. Key is the stock it is buying, while the value is the amount
+        obj.testBuy(stockAmount)
 
-    obj.testSell(stockAmount) # Sells stocks. Key is the stock it is selling, while the value is the amount
+        # Sells stocks. Key is the stock it is selling, while the value is the amount
+        obj.testSell(stockAmount)
 
     # TODO:
     # - Buy/sell stocks
@@ -37,9 +44,6 @@ def main():
         # objs = {tics: stock.Stock(credentials = credentials, ticker = tics) for tics in tickers.dticker}
         # #objs[tickers[0]].buy()
         # price_check(tickers, table, objs)'''
-    
-    obj = degiro.Degiro() # Creates an instance of degiro.Degiro()
-    print(obj.totalValue())
 
 
 if __name__ == "__main__":
